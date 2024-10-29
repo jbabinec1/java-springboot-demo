3ce4a44355c7770de681aa1a7aa3bfa486c48d7e
Jared Babinec <jared@krafton.com>
2024-10-29 12:04:49 -0400

    add project

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Jared", "Jared@email.com", 31, LocalDate.of(1993, Month.JULY, 19)  ));
+    }
+}
